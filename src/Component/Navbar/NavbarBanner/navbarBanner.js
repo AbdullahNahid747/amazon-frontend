3be54246b79da1710cfd81287e79638144d2088f
Abdullahnahid <abdullahalnahid747@gmail.com>
2024-10-25 09:06:25 +0600

    another file add

--- src/Component/Navbar/NavbarBanner/navbarBanner.js
+++ src/Component/Navbar/NavbarBanner/navbarBanner.js
@@ -1,9 +1,43 @@
 import React from 'react';
 import './navbarBanner.css'
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 const NavbarBanner = () => {
+    const options = [
+        {"name": "Fresh"},
+        {"name": "Amazon miniT"},
+        {"name": "Sell"},
+        {"name": "Best Sellers"},
+        {"name": "Today's Deals"},
+        {"name": "Mobile"},
+        {"name": "Electronics"},
+        {"name": "Prime"},
+        {"name": "Customer Service"},
+        {"name": "Fashion"},
+        {"name": "Home & Kitchen"}
+    ]
     return (
-        <div>
-            NavbarBanner
+        <div className="navbarBanner">
+            <div className="navbarBannerOptionsLeft">
+                <div className='optionsNavbarBanner'>
+                    <MenuOutlinedIcon sx={{ fontSize: "24px"}} />
+                    <div className='allOptionsNavbarBanner'> 
+                        All
+                    </div>
+                </div>
+
+                {
+                    options.map((item,ind) => {
+                        return (
+                            <div className='optionsNavbarBanner' key={ind} >
+                                <div className='allOptionsNavbarBanner'> 
+                                    {item.name}
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+
+            </div>
         </div>
     );
 }
